01efc51fb6cf138d5d9c4add8de278c242529607
Ismael <ismael@ismael-laptop.(none)>
2014-01-13 00:56:37 +0100

    setting up empty rails application

---
+++ awesome-library/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwesomeLibrary::Application.config.secret_key_base = 'ea25e766935ef6f61d5c2944e49a19359956fc8e4300e36657029e2eaba0e076f9d1a77a7ef24c50b456255499948bdfa40486327b6786cda859f58d60c61a2c'
